1edd0758e34d7e9ab52cb9e0312e79825f65a007
Luis Martinez de Bartolome <lasote@gmail.com>
2019-07-08 16:03:57 +0200

    exclude test branch

--- src/org/jfrog/conanci/TestRunner.groovy
+++ src/org/jfrog/conanci/TestRunner.groovy
@@ -20,7 +20,7 @@ class TestRunner {
         testLevelConfig.init() // This will read the tags from the PR if this is a PR
         runRESTTests()
         script.echo("Branch: ${script.env.BRANCH_NAME}")
-        if(script.env.JOB_NAME == "ConanNightly" || script.env.BRANCH_NAME == "PR-5458" || script.env.BRANCH_NAME =~ /(^release.*)|(^master)/) {
+        if(script.env.JOB_NAME == "ConanNightly" || script.env.BRANCH_NAME =~ /(^release.*)|(^master)/) {
             runReleaseTests()
         }
         else{
