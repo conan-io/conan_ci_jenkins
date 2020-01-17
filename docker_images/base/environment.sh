0e233390e5db05a892cdb701ac0f71b2d41a287b
jgsogo <jgsogo@gmail.com>
2020-01-17 17:48:30 +0100

    Dockerfile for base image

---
+++ docker_images/base/environment.sh
@@ -0,0 +1,4 @@
+export PY27="/root/.pyenv/versions/2.7.17/bin/python"
+export PY36="/root/.pyenv/versions/3.6.10/bin/python"
+export PY37="/root/.pyenv/versions/3.7.6/bin/python"
+export PY38="/root/.pyenv/versions/3.8.1/bin/python"
