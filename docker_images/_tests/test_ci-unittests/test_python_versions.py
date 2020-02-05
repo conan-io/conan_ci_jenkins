c8ec860ffd199913303c19bdf85d6ae4afc0ab05
jgsogo <jgsogo@gmail.com>
2020-02-05 23:09:27 +0100

    check python versions

--- docker_images/_tests/test_ci-unittests/test_python_versions.py
+++ docker_images/_tests/test_ci-unittests/test_python_versions.py
@@ -1,7 +1,28 @@
 import unittest
+import os
+import subprocess
+
 
 class CIUnittestsTestCase(unittest.TestCase):
+    """ Check Python versions are available and environment variables """
     
-    def test_base(self):
-        self.assertTrue(2+3, 5)
-        self.assertTrue(2+3, 5)
+    def test_py27(self):
+        python_bin = os.environ["PY27"]
+        out, _ = subprocess.Popen([python_bin, '--version'], stdout=subprocess.PIPE, shell=False).communicate()
+        self.assertTrue(out.startswith("Python 2.7"))
+
+    def test_py35(self):
+        python_bin = os.environ["PY35"]
+        out, _ = subprocess.Popen([python_bin, '--version'], stdout=subprocess.PIPE, shell=False).communicate()
+        self.assertTrue(out.startswith("Python 3.5"))
+
+    def test_py37(self):
+        python_bin = os.environ["PY37"]
+        out, _ = subprocess.Popen([python_bin, '--version'], stdout=subprocess.PIPE, shell=False).communicate()
+        self.assertTrue(out.startswith("Python 3.7"))
+
+    def test_py38(self):
+        python_bin = os.environ["PY38"]
+        out, _ = subprocess.Popen([python_bin, '--version'], stdout=subprocess.PIPE, shell=False).communicate()
+        self.assertTrue(out.startswith("Python 3.8"))
+
