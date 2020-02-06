8132a9d05167fe6f9e2f467e54bafbb385831d5e
jgsogo <jgsogo@gmail.com>
2020-02-07 00:17:32 +0100

    scripts to select alternatives

---
+++ docker_images/ci-functional/select_clang.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "usage: $0 version" 1>&2
+    exit 1
+fi
+
+if [ ! -f "/usr/bin/clang-$1" ] || [ ! -f "/usr/bin/clang++-$1" ]; then
+    echo "no such version clang/clang++ installed" 1>&2
+    exit 1
+fi
+
+update-alternatives --set clang "/usr/bin/clang-$1"
+update-alternatives --set clang++ "/usr/bin/clang++-$1"
