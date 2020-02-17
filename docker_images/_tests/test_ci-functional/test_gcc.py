74d807e444ae94fa58bd28975e296b2d67d2b999
jgsogo <jgsogo@gmail.com>
2020-02-17 21:08:36 +0100

    install several cmake versions

--- docker_images/_tests/test_ci-functional/test_gcc.py
+++ docker_images/_tests/test_ci-functional/test_gcc.py
@@ -7,8 +7,28 @@ from packaging.version import VERSION_PATTERN, parse
 from parameterized import parameterized
 
 
+gxx_7 = os.environ["GXX_7"]
+gcc_7 = os.environ["GCC_7"]
+gxx_9 = os.environ["GXX_9"]
+gcc_9 = os.environ["GCC_9"]
+
+
 class GCCTestCase(unittest.TestCase):
     """ Check GCC versions exist and work: GCC-7 and GCC-9 """
+
+    def test_gcc7_versions(self):
+        expected = "7"
+        out, _ = subprocess.Popen([gxx_7, '-dumpversion'], stdout=subprocess.PIPE).communicate()
+        self.assertEqual(out.decode().strip(), expected)
+        out, _ = subprocess.Popen([gcc_7, '-dumpversion'], stdout=subprocess.PIPE).communicate()
+        self.assertEqual(out.decode().strip(), expected)
+
+    def test_gcc9_versions(self):
+        expected = "9"
+        out, _ = subprocess.Popen([gxx_9, '-dumpversion'], stdout=subprocess.PIPE).communicate()
+        self.assertEqual(out.decode().strip(), expected)
+        out, _ = subprocess.Popen([gcc_9, '-dumpversion'], stdout=subprocess.PIPE).communicate()
+        self.assertEqual(out.decode().strip(), expected)
 
     @staticmethod
     def _grep_cplusplus(cmd):
@@ -19,7 +39,7 @@ class GCCTestCase(unittest.TestCase):
         return out.strip()
 
     def test_gcc9_default(self):
-        cmd = f"g++-9 -dM -E -x c++ /dev/null"
+        cmd = f"{gxx_9} -dM -E -x c++ /dev/null"
         out = self._grep_cplusplus(cmd.split())
         self.assertEqual(out.decode(), '#define __cplusplus 201402L')  # This is C++14
 
@@ -29,12 +49,12 @@ class GCCTestCase(unittest.TestCase):
                            ("-std=c++17", "201703L"),
                            ("-std=c++2a", "201709L"),])
     def test_gcc9(self, std_flag, std_output):
-        cmd = f"g++-9 -dM -E {std_flag} -x c++ /dev/null"
+        cmd = f"{gxx_9} -dM -E {std_flag} -x c++ /dev/null"
         out = self._grep_cplusplus(cmd.split())
         self.assertEqual(out.decode(), f'#define __cplusplus {std_output}')
 
     def test_gcc7_default(self):
-        cmd = f"g++-7 -dM -E -x c++ /dev/null"
+        cmd = f"{gxx_7} -dM -E -x c++ /dev/null"
         out = self._grep_cplusplus(cmd.split())
         self.assertEqual(out.decode(), '#define __cplusplus 201402L')  # This is C++14
 
@@ -43,7 +63,7 @@ class GCCTestCase(unittest.TestCase):
                            ("-std=c++14", "201402L"),
                            ("-std=c++17", "201703L"),])
     def test_gcc7(self, std_flag, std_output):
-        cmd = f"g++-7 -dM -E {std_flag} -x c++ /dev/null"
+        cmd = f"{gxx_7} -dM -E {std_flag} -x c++ /dev/null"
         out = self._grep_cplusplus(cmd.split())
         self.assertEqual(out.decode(), f'#define __cplusplus {std_output}')
 
