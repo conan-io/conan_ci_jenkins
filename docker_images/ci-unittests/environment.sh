512f2108061c895432f2cfe9880010d77b70c510
jgsogo <jgsogo@gmail.com>
2020-02-17 19:41:04 +0100

    py36 is not available

--- docker_images/ci-unittests/environment.sh
+++ docker_images/ci-unittests/environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PY27="/root/.pyenv/versions/2.7.17/bin/python"
-export PY36="/root/.pyenv/versions/3.6.10/bin/python"
+export PY35="/root/.pyenv/versions/3.5.9/bin/python"
 export PY37="/root/.pyenv/versions/3.7.6/bin/python"
 export PY38="/root/.pyenv/versions/3.8.1/bin/python"
