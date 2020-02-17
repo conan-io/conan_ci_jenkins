bc14c0d1b7dc4ef56f55fdc0f6273aee96af423e
jgsogo <jgsogo@gmail.com>
2020-02-17 20:13:10 +0100

    output order

--- docker_images/ci-functional/select_clang.sh
+++ docker_images/ci-functional/select_clang.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-echo "Set Clang version $1"
+echo "Set Clang version $1" 1>&2
 
 if [ ! -f "/usr/bin/clang-$1" ] || [ ! -f "/usr/bin/clang++-$1" ]; then
     echo "no such version clang/clang++ installed" 1>&2
