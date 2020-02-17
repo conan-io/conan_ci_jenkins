dce7eee6d3906efcc103ba45858692a6aa7717dc
jgsogo <jgsogo@gmail.com>
2020-02-17 21:35:59 +0100

    add comment

--- docker_images/_tests/run_tests.sh
+++ docker_images/_tests/run_tests.sh
@@ -2,3 +2,5 @@ echo "Running tests using $PYTHON"
 $PYTHON -m pip install -U pip
 $PYTHON -m pip install pytest parameterized
 $PYTHON -m pytest /home/tests/test_$IMAGE/
+
+# It is not a problem if this file modifies the image, any modification will be discarded
