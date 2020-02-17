ff7605b66bee139a6b63fa1d3ad26f6a27401669
jgsogo <jgsogo@gmail.com>
2020-02-17 20:11:48 +0100

    proper testing to get the std

--- docker_images/ci-functional/select_alternatives.sh
+++ docker_images/ci-functional/select_alternatives.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "usage: $0 gcc $1 $2 clang $3 $4" 1>&2
+# echo "usage: $0 gcc $1 $2 clang $3 $4" 1>&2
 
 if [[ $1 != gcc || $3 != clang ]]; then
     echo "usage: $0 gcc <version> clang <version>" 1>&2
@@ -10,4 +10,9 @@ fi
 /root/select_gcc.sh $2
 /root/select_clang.sh $4
 
-/bin/bash
+# Pass the rest of the arguments to /bin/bash
+all_args=("$@")
+rest_args=("${all_args[@]:4}")
+echo "${rest_args[@]}"
+
+/bin/bash "${rest_args[@]}"
--- docker_images/ci-unittests/environment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PY27="/root/.pyenv/versions/2.7.17/bin/python"
-export PY35="/root/.pyenv/versions/3.5.9/bin/python"
-export PY37="/root/.pyenv/versions/3.7.6/bin/python"
-export PY38="/root/.pyenv/versions/3.8.1/bin/python"
