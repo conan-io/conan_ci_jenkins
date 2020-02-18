c756afd1ef3557255e3fa90e3fba8808f4583ebd
jgsogo <jgsogo@gmail.com>
2020-02-18 11:02:14 +0100

    run outside tests

--- docker_images/_tests/run_tests.sh
+++ docker_images/_tests/run_tests.sh
@@ -1,6 +1,6 @@
 echo "Running tests using $PYTHON"
 $PYTHON -m pip install -U pip
 $PYTHON -m pip install pytest parameterized
-$PYTHON -m pytest -v -m "not outside" /home/tests/test_$IMAGE/
+$PYTHON -m pytest -m "not outside" /home/tests/test_$IMAGE/
 
 # It is not a problem if this file modifies the image, any modification will be discarded
