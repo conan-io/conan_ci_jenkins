9319172d76ea45c627cebf9acbbe712f36aeb950
jgsogo <jgsogo@gmail.com>
2020-02-18 10:20:05 +0100

    path to the tests

--- docker_images/_tests/run_outside_tests.sh
+++ docker_images/_tests/run_outside_tests.sh
@@ -1,6 +1,6 @@
 echo "Running from outside docker image $IMAGE"
 python3 -m pip install -U pip
 python3 -m pip install pytest parameterized
-python3 -m pytest -m outside /home/tests/test_$IMAGE/
+python3 -m pytest -m outside $1
 
 # It is not a problem if this file modifies the image, any modification will be discarded
