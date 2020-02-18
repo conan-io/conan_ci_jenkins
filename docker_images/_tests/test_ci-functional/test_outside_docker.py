21141d67053a4761137cd2d0dd2f0381b6fd62b1
jgsogo <jgsogo@gmail.com>
2020-02-18 11:02:14 +0100

    run outside tests

--- docker_images/_tests/test_ci-functional/test_outside_docker.py
+++ docker_images/_tests/test_ci-functional/test_outside_docker.py
@@ -1,8 +1,23 @@
+import os
+import re
+import subprocess
 import unittest
+
+from packaging.version import VERSION_PATTERN, parse
+from parameterized import parameterized
+
 import pytest
 
 
 @pytest.mark.outside
 class OutsideDockerTests(unittest.TestCase):
-    def test_entrypoint(self):
-        self.fail("Not expected to run")
+
+    @pytest.mark.skipif(os.environ["IMAGE"] != "ci-unittests", reason="Tests only for image 'ci-unittests'")
+    def test_unittests_image(self):
+        out, _ = subprocess.Popen(['docker', 'run', os.environ["IMAGE"], '-c', 'cat /etc/lsb-release'], stdout=subprocess.PIPE, shell=False).communicate()
+        self.assertIn("DISTRIB_ID=Ubuntu", out.decode())
+
+    @pytest.mark.skipif(os.environ["IMAGE"] != "ci-functional", reason="Tests only for image 'ci-functional'")
+    def test_functional_image(self):
+        out, _ = subprocess.Popen(['docker', 'run', os.environ["IMAGE"], 'gcc', '7', 'clang', '9', 'cmake', '3.16.4', '-c', 'cat /etc/lsb-release'], stdout=subprocess.PIPE, shell=False).communicate()
+        self.assertIn("DISTRIB_ID=Ubuntu", out.decode())
