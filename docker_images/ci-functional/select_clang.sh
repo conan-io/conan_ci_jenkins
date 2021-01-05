cd9e6613983c255e3ff54d628541d654201c76e2
czoido <mrgalleta@gmail.com>
2021-01-05 09:34:49 +0100

    add functional image

---
+++ docker_images/ci-functional/select_clang.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "usage: $0 <version>" 1>&2
+    exit 1
+fi
+
+echo "Set Clang version $1" 1>&2
+
+if [ ! -f "/usr/bin/clang-$1" ] || [ ! -f "/usr/bin/clang++-$1" ]; then
+    echo "no such version clang/clang++ installed" 1>&2
+    exit 1
+fi
+
+update-alternatives --set clang "/usr/bin/clang-$1"
+update-alternatives --set clang++ "/usr/bin/clang++-$1"
