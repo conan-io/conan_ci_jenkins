f2e09ac4a1249b42a8c4e6bdb5b78c2e226dbcd0
Luis Caro Campos <3535649+jcar87@users.noreply.github.com>
2022-08-16 10:30:25 +0100

    Fix syntax error

--- src/org/jfrog/conanci/TestRunner.groovy
+++ src/org/jfrog/conanci/TestRunner.groovy
@@ -151,7 +151,7 @@ class TestRunner {
                             if (slaveLabel == "Windows") {
                                 script.bat(script: cmd)
                             }
-                            else if (slaveLabel == "Macos" {
+                            else if (slaveLabel == "Macos") {
                                 script.sh(script: cmd)
                             }
                             else if (slaveLabel == "M1Macos") {
