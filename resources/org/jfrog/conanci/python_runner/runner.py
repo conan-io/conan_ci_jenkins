f1e9c8fe79acbb8b6f952a6027471415bbdd5e3b
czoido <mrgalleta@gmail.com>
2023-12-20 18:16:02 +0100

    fix venv call

--- resources/org/jfrog/conanci/python_runner/runner.py
+++ resources/org/jfrog/conanci/python_runner/runner.py
@@ -55,21 +55,14 @@ def run_tests(module_path, pyver, source_folder, tmp_folder, flavor, excluded_ta
     pip_installs += "pip install meson --upgrade && "
 
     #  --nocapture
-    command = "virtualenv --python \"{pyenv}\" \"{venv_dest}\" && " \
-              "{source_cmd} \"{venv_exe}\" && " \
-              "{pip_installs} " \
-              "python setup.py install && " \
-              "conan --version && conan --help && " \
-              "pytest -rf {module_path} {tags_str} " \
-              "{multiprocess} --durations=20".format(**{"module_path": module_path,
-                                                        "pyenv": pyenv,
-                                                        "tags_str": tags_str,
-                                                        "venv_dest": venv_dest,
-                                                        "verbosity": verbosity,
-                                                        "venv_exe": venv_exe,
-                                                        "source_cmd": source_cmd,
-                                                        "multiprocess": multiprocess,
-                                                        "pip_installs": pip_installs})
+    venv = "virtualenv" if platform.system() != "Darwin" else f"{pyenv} -m virtualenv"
+    command = f"{venv} --python \"{pyenv}\" \"{venv_dest}\" && " \
+              f"{source_cmd} \"{venv_exe}\" && " \
+              f"{pip_installs} " \
+              f"python setup.py install && " \
+              f"conan --version && conan --help && " \
+              f"pytest -rf {module_path} {tags_str} " \
+              f"{multiprocess} --durations=20"
 
     env = get_environ(tmp_folder)
     env["PYTHONPATH"] = source_folder
